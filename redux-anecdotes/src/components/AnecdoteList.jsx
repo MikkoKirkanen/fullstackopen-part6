2eac2fa0670c3754c641641434fe3adc9023f182
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2024-12-05 14:33:38 +0200

    Exercise 6.9.

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,13 +2,18 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => {
+    const regexp = new RegExp(state.filter, 'i')
+    return !state.filter
+      ? state.anecdotes
+      : state.anecdotes.filter((a) => regexp.test(a.content))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
   }
-  
+
   return (
     <>
       {anecdotes.map((anecdote) => (
